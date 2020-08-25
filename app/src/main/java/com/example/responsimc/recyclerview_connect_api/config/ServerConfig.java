869c22b661dcfe19779184f197bf222f6ae494ae
FajarBagus22 <bagusdajar565@gmail.com>
2020-08-26 06:23:07 +0700

    Praktikum Mobile Computing

---
+++ app/src/main/java/com/example/responsimc/recyclerview_connect_api/config/ServerConfig.java
@@ -0,0 +1,7 @@
+package com.example.responsimc.recyclerview_connect_api.config;
+
+public class ServerConfig {
+    public static final String URL_BASE = "https://api.themoviedb.org";
+    public static final String API_ENDPOINT = URL_BASE;
+
+}
